20c79718b97d6849a877617ebb2e86c7fc93080d
Sandeep Singh <serperior2@protonmail.com>
2024-03-04 22:25:42 -0500

    home page style update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: [
-		{
-			mytheme: {
-				"primary": "#a991f7",
-				"secondary": "#f6d860",
-				"accent": "#37cdbe",
-				"neutral": "#3d4451",
-				"base-100": "#ffffff",
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					"primary": "#a991f7",
+					"secondary": "#f6d860",
+					"accent": "#37cdbe",
+					"neutral": "#3d4451",
+					"base-100": "#ffffff",
+				},
 			},
-		},
-	],
+		]
+	},
+	theme: [],
 	plugins: [require("@tailwindcss/typography"), require("daisyui")],
 }
