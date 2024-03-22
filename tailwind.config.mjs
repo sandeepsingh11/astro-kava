dbff2de4b208b548f8737a4e28fbfb30c52dbdd5
Sandeep Singh <serperior2@protonmail.com>
2024-03-22 19:31:39 -0400

    removed daisyui, use flowbite components, Home page restyle

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	daisyui: {
-		themes: [
-			{
-				mytheme: {
-					"primary": "#a991f7",
-					"secondary": "#f6d860",
-					"accent": "#37cdbe",
-					"neutral": "#3d4451",
-					"base-100": "#ffffff",
-				},
-			},
-		]
-	},
 	theme: [],
-	plugins: [require("@tailwindcss/typography"), require("daisyui")],
+	plugins: [require("@tailwindcss/typography")],
 }
